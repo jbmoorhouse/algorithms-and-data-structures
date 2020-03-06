6f4b5b865a11c6f7a4ffe597309ef26d63a30cc9
Joseph <j.moorhouse1898@gmail.com>
2020-03-06 20:54:01 +0000

    Added simple node definition

--- pyads/linked_list/node.py
+++ pyads/linked_list/node.py
@@ -1,5 +1,15 @@
 """
 """
-
+# =============================================================================
 class Node:
-    pass
+    def __init__(self, data, **pointers):
+        self.data = data
+            
+    def __setattr__(self, name, value):
+        if name == 'data' and isinstance(value, int):
+            pass
+        elif not (isinstance(value, Node) or value is None):
+            raise TypeError(f"Bad type for {name}, detected {type(value)}")
+        
+        self.__dict__[name] = value
+
