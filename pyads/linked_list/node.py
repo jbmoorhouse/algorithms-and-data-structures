1113a46c43f85e34ca5a784159d56f41e5f28777
Joseph <j.moorhouse1898@gmail.com>
2020-03-07 13:07:10 +0000

    Added simple docstring

--- pyads/linked_list/node.py
+++ pyads/linked_list/node.py
@@ -3,6 +3,23 @@
 # =============================================================================
 class Node:    
     def __init__(self, data, **pointers):
+        """Data structure node which be used in applications such as linked
+        list, trees etc.
+        
+        Parameters
+        ----------
+        data : int or float
+            Simple node contains only numerical data
+        pointers : Node or None
+            Set an arbritary number of pointers, depending on the data 
+            structure type. E.g. Linked list may contain ``next`` and 
+            ``prev``. A tree may contain ``left`` and ``right`` etc
+
+        Examples
+        --------
+        n1 = Node(data = 10)
+        n2 = Node(data = 5, left = n1)
+        """
         self.data = data
         
         for k, v in pointers.items():
@@ -11,7 +28,7 @@ class Node:
     def __repr__(self):
         attr = ", ".join(
             f"{attr}={value!r}" for attr, value in self.__dict__.items())
-            
+
         return f"{self.__class__.__name__}({attr})"
                 
     def __setattr__(self, name, value):
