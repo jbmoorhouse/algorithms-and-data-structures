88a4f9e33b5ed6120bb54aa84db120be78be08d0
Joseph <j.moorhouse1898@gmail.com>
2020-04-03 20:13:10 +0100

    Added compile arguments

--- pyads/utils/setup.py
+++ pyads/utils/setup.py
@@ -7,6 +7,8 @@ def compile_cython_files():
         name = "_mutation",
         ext_modules = cythonize("_mutation.pyx"),
         include_dirs=[np.get_include()],
+        extra_compile_args=['-fopenmp'],
+        extra_link_args=['-fopenmp']
     )
 
 if __name__ == "__main__":
